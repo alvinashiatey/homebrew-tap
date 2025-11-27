680032211a10711cc02ae5b47049a66567142a82
Alvin Ashiatey <alvinashiatey@gmail.com>
2025-11-27 15:00:15 -0500

    Fix sha256 checksum in r110 Cask

--- Casks/R110.rb
+++ Casks/R110.rb
@@ -1,6 +1,6 @@
 cask "r110" do
   version "0.1.0"
-  sha256 "57744f926aaf472f2d4b5370064f4c94e0c7711fc5e2a7d176d020ab54e55ef2"
+  sha256 "77637602a17aaa3fe00c0ac9e8bdcb0a340b07c1c6c3eacf062d143ec6a9175e"
 
   url "https://github.com/alvinashiatey/R110/releases/download/v#{version}/R110_#{version}_aarch64.dmg"
   name "R110"

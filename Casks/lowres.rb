3f9a88fa03ce6b45a2e827ea011d848f0c77148f
Alvin Ashiatey <alvinashiatey@gmail.com>
2025-11-26 23:44:44 -0500

    Fix download URL format in lowres Cask

--- Casks/lowres.rb
+++ Casks/lowres.rb
@@ -2,7 +2,7 @@ cask "lowres" do
   version "0.1.0"
   sha256 "0f9f22e1191a4b001c8756729a06035aaff4830f9095631e8e67f0d1befd7d03"
 
-  url "https://github.com/alvinashiatey/lowres/releases/download/v#{version}/lowres_#{version}_aarch64.dmg"
+  url "https://github.com/alvinashiatey/lowres/releases/download/app-v#{version}/lowres_#{version}_aarch64.dmg"
   name "lowres"
   desc "Reduce image resolution and file size"
   homepage "https://github.com/alvinashiatey/lowres"

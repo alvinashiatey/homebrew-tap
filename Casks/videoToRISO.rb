bf163a4c14bfac8a9aea70b66d4ebd3c7aef6367
Alvin Ashiatey <alvinashiatey@gmail.com>
2025-12-03 22:16:35 -0500

    v0.2.0

--- Casks/videoToRISO.rb
+++ Casks/videoToRISO.rb
@@ -1,6 +1,6 @@
 cask "videotoriso" do
-  version "0.1.2"
-  sha256 "38bb3c5d57721982b1e58afb518de3e5555219b82a0316368657e094d594091e"
+  version "0.2.0"
+  sha256 "0b768eb573485bd4f89e34eb1e9e2984ac08e53ee1dec935f5b15fa4ae8feb44"
 
   url "https://github.com/alvinashiatey/videoToRISO/releases/download/v#{version}/VideoToRISO.zip"
   name "VideoToRISO"
